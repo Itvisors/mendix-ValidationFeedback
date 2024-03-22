84dc6d4f44ed21db541f9d8d21526a8717b7cee1
Johan Munneke <johanmunneke94@gmail.com>
2024-03-22 09:51:52 +0100

    First version widget

--- src/ValidationFeedback.jsx
+++ src/ValidationFeedback.jsx
@@ -1,8 +1,25 @@
 import { createElement } from "react";
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
+import { Alert } from "./components/Alert";
 import "./ui/ValidationFeedback.css";
 
-export function ValidationFeedback({ sampleText }) {
-    return <HelloWorldSample sampleText={sampleText} />;
+export function ValidationFeedback({ attribute, association, alertStyle, class: classNameInput, style: styleInput }) {
+    // initialize validationFeedback, which can be either undefined or a string
+    let validationFeedbackAttribute;
+    let validationFeedbackAssociation;
+    // Check whether there is a validation message set for the attribute or association used
+    if (attribute && typeof attribute.validation !== "undefined") {
+        validationFeedbackAttribute = attribute.validation;
+    }
+    if (association && typeof association.validation !== "undefined") {
+        validationFeedbackAssociation = association.validation;
+    }
+    let style = alertStyle ? alertStyle.value : undefined;
+    let className = "custom-validation-widget " + (classNameInput ? classNameInput : "");
+    return validationFeedbackAttribute || validationFeedbackAssociation ? (
+        <div className={className} style={styleInput}>
+            <Alert alertStyle={style}>{validationFeedbackAttribute}</Alert>
+            <Alert alertStyle={style}>{validationFeedbackAssociation}</Alert>
+        </div>
+    ) : undefined;
 }
---
+++ src/components/Alert.jsx
@@ -0,0 +1,26 @@
+import { createElement } from "react";
+import classNames from "classnames";
+
+/**
+ * alertStyle = "default" | "primary" | "success" | "info" | "warning" | "danger"
+ * className = custom className to be added
+ * children = content to show within alert
+ */
+export const Alert = ({ alertStyle = "danger", className, children }) => {
+    let alertClass;
+    if (
+        alertStyle == "default" ||
+        alertStyle == "primary" ||
+        alertStyle == "success" ||
+        alertStyle == "info" ||
+        alertStyle == "warning" ||
+        alertStyle == "danger"
+    ) {
+        alertClass = "alert-" + alertStyle;
+    } else {
+        alertClass = "alert-danger";
+    }
+    return children ? (
+        <div className={classNames(`alert ${alertClass} mx-validation-message`, className)}>{children}</div>
+    ) : null;
+};
